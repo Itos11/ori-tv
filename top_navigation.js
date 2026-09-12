84d9106507bf5be5c33273304077fbe24d9c2c3e
Itos11 <bqrt30@gmail.com>
2026-09-12 13:21:03 +0300

    Update top_navigation.js

--- top_navigation.js
+++ top_navigation.js
@@ -1,21 +1,10 @@
-(function () {
-    'use strict';
+(function(){
 
-    function start() {
-        console.log('Lampa Top Navigation: OK');
-
-        if (Lampa.Noty && Lampa.Noty.show) {
-            Lampa.Noty.show('TOP NAVIGATION: ПЛАГИН ЗАГРУЖЕН');
-        }
+    if (!window.Lampa) {
+        alert('LAMPA NOT FOUND');
+        return;
     }
 
-    if (window.appready) {
-        start();
-    } else if (Lampa.Listener && Lampa.Listener.follow) {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                start();
-            }
-        });
-    }
+    alert('PLUGIN WORKS');
+
 })();
